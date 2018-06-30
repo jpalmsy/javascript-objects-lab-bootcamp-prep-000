0e189f0a7231884fff53288ae3378fe7dcf15ef8
Joey Palmieri <joeypalmieri@hotmail.com>
2018-06-30 22:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
